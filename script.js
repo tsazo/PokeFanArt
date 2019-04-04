3a0c07eef066ab5eefe0f339268d76eb5ebe931c
Trinity Sazo <tsazo@andover.edu>
2019-04-04 18:44:51 -0400

    Upload script.js
    Uploaded and made script.js for both the index.html and content.html pages to use

---
+++ script.js
@@ -0,0 +1,134 @@
+// Learned a bit of JS from before, and this year from using CodeCademy
+
+//All fuctions below are for their respective buttons
+function one() {
+  let cite = document.getElementById('one');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function two() {
+  let cite = document.getElementById('two');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function three() {
+  let cite = document.getElementById('three');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function four() {
+  let cite = document.getElementById('four');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function five() {
+  let cite = document.getElementById('five');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function six() {
+  let cite = document.getElementById('six');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function seven() {
+  let cite = document.getElementById('seven');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function eight() {
+  let cite = document.getElementById('eight');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function nine() {
+  let cite = document.getElementById('nine');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function ten() {
+  let cite = document.getElementById('ten');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function eleven() {
+  let cite = document.getElementById('eleven');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
+
+function twelve() {
+  let cite = document.getElementById('twelve');
+  if (cite.style.display === 'block') {
+    cite.style.display = 'none';
+    event.target.innerHTML = 'Click to show citation';
+  } else {
+      cite.style.display = 'block';
+      event.target.innerHTML = 'Click to hide citation';
+  }
+}
